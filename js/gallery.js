c47bc5bb4757eed10b809024006e71dc180ca34d
lina dorasheva <dorasheva.a@gmail.com>
2019-10-06 20:45:05 +0300

    Добавляем функцию загрузки фотографий других пользователей с сервера

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,11 @@
 (function () {
   // Контейнер для фотографий других пользователей
   var picturesBlock = document.querySelector('.pictures');
+  var errorTemplate = document.querySelector('#error').content;
+  var mainBlock = document.querySelector('main');
 
-  // Функция заполнения блока DOM-элементами на основе массива JS-объектов
-  var renderPictures = function (array) {
+  // Отрисовываем фото, загруженные с сервера (в случае успешно-обработанного запроса)
+  var onSuccess = function (array) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < array.length; i++) {
       fragment.appendChild(window.pictures.createPicture(array[i]));
@@ -13,5 +15,18 @@
     picturesBlock.appendChild(fragment);
   };
 
-  renderPictures(window.pictures.pictures);
+  // Получаем сообщение об ошибке из шаблона
+  var createError = function () {
+    var element = errorTemplate.cloneNode(true);
+
+    return element;
+  };
+
+  // Сообщение при ошибочно-обработанном запросе
+  var onError = function (message) {
+    mainBlock.appendChild(createError());
+    document.querySelector('.error__title').textContent = message;
+  };
+
+  window.load(onSuccess, onError);
 })();
