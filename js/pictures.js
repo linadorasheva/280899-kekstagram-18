cd7b954113ee29bc61dff030435c7e6e0f40e247
lina dorasheva <dorasheva.a@gmail.com>
2019-10-06 20:50:38 +0300

    Удаляет неиспользуемый код (генерирующий массив фотографий)

--- js/pictures.js
+++ js/pictures.js
@@ -3,25 +3,6 @@
 (function () {
   var templatePicture = document.querySelector('#picture').content;
 
-  var QUANTITY_OBJECTS = 25;
-  var MIN_LIKES = 15;
-  var MAX_LIKES = 200;
-
-  // Функция, возвращающая массив объектов-фотографий
-  var getPictures = function () {
-    var pictures = [];
-    for (var i = 0; i < QUANTITY_OBJECTS; i++) {
-      pictures[i] = {
-        url: 'photos/' + (i + 1) + '.jpg',
-        description: 'описание' + i,
-        likes: window.data.getRandomInteger(MIN_LIKES, MAX_LIKES),
-        comments: window.bigPicture.getComments(),
-        quantityComments: (window.bigPicture.getComments()).length
-      };
-    }
-    return pictures;
-  };
-
   var createPicture = function (object) {
     var element = templatePicture.cloneNode(true);
 
@@ -33,9 +14,6 @@
   };
 
   window.pictures = {
-
-    createPicture: createPicture,
-
-    pictures: getPictures(QUANTITY_OBJECTS)
+    createPicture: createPicture
   };
 })();
