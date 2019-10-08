571b9dbb03b7b9a3e47c7252c720c5600c32404e
angelina.dorasheva <dorasheva.a@gmail.com>
2019-10-08 21:31:55 +0300

    Вешает обработчики на миниатюры для открытия full-screen фото

--- js/gallery.js
+++ js/gallery.js
@@ -6,6 +6,20 @@
   var errorTemplate = document.querySelector('#error').content;
   var mainBlock = document.querySelector('main');
 
+  var addListeners = function () {
+    var picturesList = document.querySelectorAll('.picture');
+    for (var j = 0; j < picturesList.length; j++) {
+      picturesList[j].addEventListener('click', function (evt) {
+        evt.preventDefault();
+        if (evt.target.tagName === 'IMG') {
+          window.bigPicture.bigPictureOpen(evt);
+        }
+      }, true);
+
+      picturesList[j].addEventListener('keydown', window.bigPicture.onEnterPress);
+    }
+  };
+
   // Отрисовываем фото, загруженные с сервера (в случае успешно-обработанного запроса)
   var onSuccess = function (array) {
     var fragment = document.createDocumentFragment();
@@ -13,8 +27,8 @@
       fragment.appendChild(window.pictures.createPicture(array[i]));
     }
     picturesBlock.appendChild(fragment);
+    addListeners();
   };
-
 
   // Получаем сообщение об ошибке из шаблона
   var createError = function () {
@@ -31,3 +45,4 @@
 
   window.load(onSuccess, onError);
 })();
+
